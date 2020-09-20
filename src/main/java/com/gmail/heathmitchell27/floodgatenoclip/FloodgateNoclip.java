cdd14a528e62e4acb7a858e2747f773d7c707058
Hellohi3654 <57390764+Hellohi3654@users.noreply.github.com>
2020-09-20 22:06:24 +1000

    Update FloodgateNoclip.java

--- src/main/java/com/gmail/heathmitchell27/floodgatenoclip/FloodgateNoclip.java
+++ src/main/java/com/gmail/heathmitchell27/floodgatenoclip/FloodgateNoclip.java
@@ -1,7 +1,7 @@
 package com.gmail.heathmitchell27.floodgatenoclip;
 
-import net.minecraft.server.v1_16_R3.DedicatedServerProperties;
-import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
+import net.minecraft.server.v1_16_R2.DedicatedServerProperties;
+import org.bukkit.craftbukkit.v1_16_R2.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.spigotmc.SpigotConfig;
 
@@ -9,22 +9,6 @@ import java.lang.reflect.Field;
 import java.util.logging.Logger;
 
 public class FloodgateNoclip extends JavaPlugin {
-
-    @Override
-    public void onLoad() {
-        try {
-            DedicatedServerProperties properties = ((CraftServer) getServer()).getServer().getDedicatedServerProperties();
-            Field allowFlight = properties.getClass().getDeclaredField("allowFlight");
-            allowFlight.setAccessible(true);
-            allowFlight.set(properties, true);
-        } catch (Exception e) {
-            getLogger().warning("floodgate-noclip could not set allow-flight value!");
-            e.printStackTrace();
-        }
-        SpigotConfig.movedTooQuicklyMultiplier = 100;
-        SpigotConfig.movedWronglyThreshold = 100;
-        getLogger().info("Set config options for floodgate-noclip!");
-    }
 
     @Override
     public void onEnable() {
